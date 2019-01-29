42b97a2b991d5c02c2d78fda901b9908219fcd32
Remy Elbez <arnaconline@gmail.com>
2019-01-29 17:50:22 -0500

    changed package name

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  name: 'uiba:accounts-microsoft',
-  version: '0.0.1',
+  name: 'uibalabs:accounts-microsoft',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Login service for Microsoft (and AzureAD), using the OAuth 2.0 protocol.',
   // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.6.1.1');
+  api.versionsFrom('1.2.1');
   api.use('ecmascript');
 
   api.use(['underscore', 'random']);
@@ -20,7 +20,7 @@ Package.onUse(function(api) {
   api.imply('accounts-base', ['client', 'server']);
 
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('uiba:microsoft@0.0.1', ['client', 'server']);
+  api.use('uibalabs:microsoft@1.0.0', ['client', 'server']);
 
   api.addFiles('accounts-microsoft_login_button.css', 'client');
 
@@ -31,6 +31,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('uiba:accounts-microsoft');
+  api.use('uibalabs:accounts-microsoft@1.0.0');
   api.mainModule('accounts-microsoft-tests.js');
 });
